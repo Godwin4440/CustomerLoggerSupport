2dd6d221961aaf1fdf997f6ed3453f4db8297cc0
58390_Godwin <godwinkamble4440@gmail.com>
2022-08-21 20:03:28 +0530

    Add project files.

---
+++ CustomerLoggerSupport/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomerLoggerSupport.Startup))]
+namespace CustomerLoggerSupport
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
